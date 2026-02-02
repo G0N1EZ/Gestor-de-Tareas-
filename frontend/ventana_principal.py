c6d7e159ff91d47e359270a6ab1f779e12a2bfca
G0N1EZ <cgonzalez26@uc.cl>
2026-02-02 17:15:57 -0300

    Empezamos a desarrollar el objeto que contiene a las tareas y sus respectivas funcionalidades

--- frontend/ventana_principal.py
+++ frontend/ventana_principal.py
@@ -1,13 +1,55 @@
-from PyQt5.QtWidgets import QLabel, QTextEdit, QVBoxLayout, QHBoxLayout, QPushButton, QCheckBox, QWidget
+from PyQt5.QtWidgets import QLabel, QTextEdit, QVBoxLayout, QHBoxLayout, QPushButton, QCheckBox, QWidget, QScrollArea, QComboBox
 from parametros import SCREEN_WIDTH, SCREEN_HEIGHT, ORIGIN_X, ORIGIN_Y
+
+
+
+class CarpetaTareas(QWidget):
+    def __init__(self):
+        self.nombre = QLabel(self)
+        self.cerrado = True
+        self.contenido = QWidget()
+        self.boton = QPushButton("Abrir Carpeta", self)
+        self.boton_agregar_tareas = QPushButton("Agregar Tareas")
+
+        # layout
+        layout_tareas = QVBoxLayout()
+        layout_tareas.addItem(self.boton_agregar_tareas)
+        self.contenido.setLayout(QVBoxLayout())
+        layout_carpeta = QVBoxLayout()
+        layout_carpeta.addItem(self.boton)
+        layout_carpeta.addItem(self.contenido)
+        self.setLayout(layout_carpeta)
+
+        self.boton.clicked.connect(self.mostrar_carpeta)
+        self.contenido.setVisible(False)
+
+    def mostrar_carpeta(self):
+        if self.cerrado is True:
+            self.contenido.setVisible(True)
+            self.cerrado = False
+
+    def cerrar_carpeta(self):
+        if self.cerrado is False:
+            self.contenido.setVisible(False)
+            self.cerrado = False
+
+    def crear_carpeta(self, nombre_carpeta: str):
+        self.nombre.setText(nombre_carpeta)
+
+    def crear_tarea(self, nombre_tarea:str):
+        nueva_tarea = QCheckBox(nombre_tarea)
+
+
+
+
+
 
 class VentanaPrincipal(QWidget):
     def __init__(self):
         super().__init__()
         self.setGeometry(ORIGIN_X, ORIGIN_Y, SCREEN_WIDTH, SCREEN_HEIGHT)
-
+        self.boton = QPushButton("Agregar Carpeta de Tareas", self)
         # botones
-        self.botones = QCheckBox("Tarea 1", self)
 
         # Layout
         layout = QHBoxLayout(self)

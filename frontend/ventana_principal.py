77c385e44fb607f819157b880d8647c9cc8a8bcd
G0N1EZ <cgonzalez26@uc.cl>
2026-02-04 20:53:02 -0300

    agregado el metodo de crear carpeta y el nombre de la carpeta, siguiente es lograr una correcta visualizacion de la carpeta y manejo de los espacios en la aplicacion, ademas cuando se abre una carpeta se buguea y tapa el nombre

--- frontend/ventana_principal.py
+++ frontend/ventana_principal.py
@@ -1,23 +1,24 @@
-from PyQt5.QtWidgets import QLabel, QTextEdit, QVBoxLayout, QHBoxLayout, QPushButton, QCheckBox, QWidget, QScrollArea, QComboBox
-from parametros import SCREEN_WIDTH, SCREEN_HEIGHT, ORIGIN_X, ORIGIN_Y
+from PyQt5.QtWidgets import QLabel, QTextEdit, QVBoxLayout, QHBoxLayout, QPushButton, QCheckBox, QWidget, QScrollArea, QComboBox, QInputDialog
+from frontend.parametros import SCREEN_WIDTH, SCREEN_HEIGHT, ORIGIN_X, ORIGIN_Y
 
 
 
 class CarpetaTareas(QWidget):
-    def __init__(self):
-        self.nombre = QLabel(self)
+    def __init__(self, nombre: str):
+        super().__init__()
+        self.nombre = QLabel(nombre, self)
         self.cerrado = True
         self.contenido = QWidget()
         self.boton = QPushButton("Abrir Carpeta", self)
         self.boton_agregar_tareas = QPushButton("Agregar Tareas")
 
         # layout
-        layout_tareas = QVBoxLayout()
-        layout_tareas.addItem(self.boton_agregar_tareas)
-        self.contenido.setLayout(QVBoxLayout())
+        self.layout_tareas = QVBoxLayout()
+        self.layout_tareas.addWidget(self.boton_agregar_tareas)
+        self.contenido.setLayout(self.layout_tareas)
         layout_carpeta = QVBoxLayout()
-        layout_carpeta.addItem(self.boton)
-        layout_carpeta.addItem(self.contenido)
+        layout_carpeta.addWidget(self.boton)
+        layout_carpeta.addWidget(self.contenido)
         self.setLayout(layout_carpeta)
 
         self.boton.clicked.connect(self.mostrar_carpeta)
@@ -33,31 +34,32 @@ class CarpetaTareas(QWidget):
             self.contenido.setVisible(False)
             self.cerrado = False
 
-    def crear_carpeta(self, nombre_carpeta: str):
-        self.nombre.setText(nombre_carpeta)
-
-    def crear_tarea(self, nombre_tarea:str):
-        nueva_tarea = QCheckBox(nombre_tarea)
-
-
-
-
+    def crear_tarea(self, nombre_tarea: str):
+        nueva_tarea = QCheckBox(nombre_tarea, self.contenido)
+        self.layout_tareas.addWidget(nueva_tarea)
 
 
 class VentanaPrincipal(QWidget):
     def __init__(self):
         super().__init__()
         self.setGeometry(ORIGIN_X, ORIGIN_Y, SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.boton = QPushButton("Agregar Carpeta de Tareas", self)
+        boton_agregar_carpeta = QPushButton("Agregar Carpeta de Tareas")
         # botones
+        boton_agregar_carpeta.clicked.connect(self.crear_carpeta)
 
         # Layout
-        layout = QHBoxLayout(self)
+        self.layout_pagina = QHBoxLayout()
+        self.layout_pagina.addWidget(boton_agregar_carpeta)
 
         # agregar cosas al layout
-        layout.addWidget(self.botones)
-
-        self.setLayout(layout)
+        self.setLayout(self.layout_pagina)
 
     def mostrar_ventana(self):
         self.show()
+
+    def crear_carpeta(self):
+        nombre_carpeta, ok = QInputDialog.getText(self, "Nueva carpeta", "Nombre de Carpeta")
+        if ok and nombre_carpeta.strip():
+            nueva_carpeta = CarpetaTareas(nombre=nombre_carpeta)
+            self.layout_pagina.addWidget(nueva_carpeta)
+
